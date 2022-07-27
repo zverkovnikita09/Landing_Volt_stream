254e3a7ee65d9f27524f706cd29f9bbbe93db65a
zverkovnikita09 <zverkovnikita09@gmail.com>
2022-07-27 23:41:14 +0300

    effects

--- interObs.js
+++ interObs.js
@@ -7,21 +7,20 @@ const stepsObserver = new IntersectionObserver(([elem], observer)=>{
 {});
 
 const container = document.querySelector('.steps__items');
-const steps = Array.from(document.querySelectorAll('.stepItem'));
+const steps = document.querySelectorAll('.stepItem');
 let i = 0;
 const switch_steps =()=>{
-        setInterval(()=>{
+    const interval = setInterval(()=>{
         if(i==steps.length-1){
-            steps[i].classList.remove('active');
-            steps[0].classList.add('active');
-            i=0;
+            clearInterval(interval);
         }
         else{
             steps[i].classList.remove('active');
+            steps[i+1].classList.add('visible');
             steps[i+1].classList.add('active');
             i++;
         }
-    },3000);
+    },1000);
 }
 
 stepsObserver.observe(container);
